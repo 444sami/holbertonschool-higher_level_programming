8b0071e12aab6b5aff6928b3ae075b2efba8754b
Sami <thaisssa666@gmail.com>
2023-06-09 16:40:40 -0700

    common elements

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return set_1 & set_2
+# operador & nos retorna otro conjunto que solo contiene
+# los elementos que están presentes en ambos a la vez
