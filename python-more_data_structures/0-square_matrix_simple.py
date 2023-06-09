58c7309468a99e8f1e21ab15976daa7643202400
Sami <thaisssa666@gmail.com>
2023-06-09 16:28:54 -0700

    omg

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if matrix is None:
-        return
-    matriz = []
-    for i in matrix:
-        n = list(map(lambda x: x ** 2, matrix[i]))
-        matriz.append(n)
-    return matriz
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
